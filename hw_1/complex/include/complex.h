eec16e5b53441a683dd3cfc2d9dccd17a9d51f12
Eyan Eubanks <eyaneubanks@gmail.com>
2026-01-24 16:06:39 -0800

    - Add test make target to build test scripts. - Built and Ran Add test script. - Update Root .gitignore.

--- hw_1/complex/include/complex.h
+++ hw_1/complex/include/complex.h
@@ -1,30 +1,35 @@
+#ifndef _COMPLEX
+#define _COMPLEX
+
 #include <stdio.h>
 
 struct complex{
     double real;
     double im;
-} complex;
+};
 
 struct polar{
     double r;
     double theta;
-} polar;
+};
 
 // Basic Operators
-struct complex add(struct complex, struct complex);
-struct complex subtract(struct complex, struct complex);
-struct complex negate(struct complex, struct complex);
-struct complex multiply(struct complex, struct complex);
-struct complex divide(struct complex, struct complex);
-struct complex conjugate(struct complex, struct complex);
-struct polar to_polar(struct complex, struct complex);
-struct complex power(struct complex, struct complex);
-// Magnitude for complex and polar number
-double magnitude_complex(struct complex);
-double magnitude_polar(struct polar);
+struct complex add(struct complex a, struct complex b);
+// struct complex subtract(struct complex, struct complex);
+// struct complex negate(struct complex);
+// struct complex multiply(struct complex, struct complex);
+// struct complex divide(struct complex, struct complex);
+// struct complex conjugate(struct complex, struct complex);
+// struct polar to_polar(struct complex, struct complex);
+// struct complex power(struct complex, struct complex);
+// // Magnitude for complex and polar number
+// double magnitude_complex(struct complex);
+// double magnitude_polar(struct polar);
 
-// Comparisons
-int equals(struct complex, struct complex);
-int is_real(struct complex, struct complex);
-int is_imaginary(struct complex);
-int is_zero(struct complex);
+// // Comparisons
+// int equals(struct complex, struct complex);
+// int is_real(struct complex, struct complex);
+// int is_imaginary(struct complex);
+// int is_zero(struct complex);
+
+#endif
--- hw_1/complex/src/complex.c
+++ hw_1/complex/src/complex.c
@@ -2,14 +2,6 @@
 #include <math.h>
 #include "complex.h"
 
-int main(){
-    struct complex a = {1.12, 2.3};
-    struct complex b = {3.14, 4.17};
-
-    struct complex c = add(a, b);
-    printf("Real part is %f, and the imaginary part is %f", c.real, c.im);
-}
-
 // Adds two complex numbers together. Returns a complex number.
 struct complex add(struct complex a, struct complex b){
     struct complex c;
@@ -18,35 +10,35 @@ struct complex add(struct complex a, struct complex b){
     return c;
 };
 
-// Adds subtracts complex numbers together. Returns a complex number.
-struct complex subtract(struct complex a, struct complex b){
-    struct complex c;
-    c.real = a.real - b.real;
-    c.im = a.im - b.im;
-    return c;
-};
+// // Adds subtracts complex numbers together. Returns a complex number.
+// struct complex subtract(struct complex a, struct complex b){
+//     struct complex c;
+//     c.real = a.real - b.real;
+//     c.im = a.im - b.im;
+//     return c;
+// };
 
-// Makes a complex number negaitve.
-struct complex negate(struct complex a){
-    struct complex c;
-    c.real = (-1)*a.real;
-    c.im = (-1)*a.im;
-    return c;
-}
+// // Makes a complex number negaitve.
+// struct complex negate(struct complex a){
+//     struct complex c;
+//     c.real = (-1)*a.real;
+//     c.im = (-1)*a.im;
+//     return c;
+// };
 
-struct complex multiply(struct complex, struct complex);
-struct complex divide(struct complex, struct complex);
-struct complex conjugate(struct complex, struct complex);
-struct polar to_polar(struct complex, struct complex);
-struct complex power(struct complex, struct complex);
-// Magnitude for complex and polar number
-double magnitude_complex(struct complex);
-double magnitude_polar(struct polar);
+// struct complex multiply(struct complex, struct complex);
+// struct complex divide(struct complex, struct complex);
+// struct complex conjugate(struct complex, struct complex);
+// struct polar to_polar(struct complex, struct complex);
+// struct complex power(struct complex, struct complex);
+// // Magnitude for complex and polar number
+// double magnitude_complex(struct complex);
+// double magnitude_polar(struct polar);
 
-// Comparisons
-int equals(struct complex, struct complex);
-int is_real(struct complex, struct complex);
-int is_imaginary(struct complex);
-int is_zero(struct complex);
+// // Comparisons
+// int equals(struct complex, struct complex);
+// int is_real(struct complex, struct complex);
+// int is_imaginary(struct complex);
+// int is_zero(struct complex);
 
 
